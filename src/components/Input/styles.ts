391533621b83702fb21a3fd092100c6cb026b9e4
Fernando Henrique <liquuid@gmail.com>
2020-09-27 17:35:43 -0300

    form validation

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,33 @@
-import styled from 'styled-components';
+import styled, { css}  from 'styled-components';
+interface ContainerProps{
+    isFocused: boolean;
+    isFilled: boolean;
+}
+export const Container = styled.div<ContainerProps>`
 
-export const Container = styled.div`
+        margin-top: 24px;
+        background: #232129;
+        border-radius: 10px;
 
-            margin-top: 24px;
-            background: #232129;
-            border-radius: 10px;
-            border: 2px solid #232129;
-            color: #666360;
-            padding: 16px;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            & + div {
-                margin-top: 8px;
-            }
+        border: 2px solid #232129;
+        color: #666360;
+
+        padding: 16px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        & + div {
+            margin-top: 8px;
+        }
+        ${props => props.isFocused && css`
+            color: #FF9000;
+            border: 2px solid #FF9000;
+        `}
+
+        ${props => props.isFilled && css`
+            color: #FF9000;
+
+        `}
 
         input {
             background: transparent;
