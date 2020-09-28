252761ea21a2ac2edbc56aec8ac2216e4461f11c
Fernando Henrique <liquuid@gmail.com>
2020-09-27 21:54:22 -0300

    add tooltips and  form validations

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,10 @@
 import styled, { css}  from 'styled-components';
+import Tooltip from '../Tooltip';
+
 interface ContainerProps{
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 export const Container = styled.div<ContainerProps>`
 
@@ -19,6 +22,12 @@ export const Container = styled.div<ContainerProps>`
         & + div {
             margin-top: 8px;
         }
+
+        ${props => props.isErrored && css`
+            border: 2px solid #c53030;
+
+        `}
+
         ${props => props.isFocused && css`
             color: #FF9000;
             border: 2px solid #FF9000;
@@ -28,6 +37,7 @@ export const Container = styled.div<ContainerProps>`
             color: #FF9000;
 
         `}
+
 
         input {
             background: transparent;
@@ -43,4 +53,21 @@ export const Container = styled.div<ContainerProps>`
         svg {
             margin-right: 16px;
         }
+
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+    margin-left: 16px;
+    svg {
+        margin: 0;
+    }
+    span {
+        background: #c53030;
+        color: #fff;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
+    }
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useCallback, useRef} from 'react';
 import {FiLogIn , FiMail , FiLock } from 'react-icons/fi';
-import logoImg from '../../assets/logo.svg';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-<Container>
-    <Content>
-        <img src={logoImg} alt="Easy Barber"/>
-        <form>
-            <h1>Faça seu Logon</h1>
+const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
 
-            <Input name="email" icon={FiMail} placeholder="E-Mail"/>
-            <Input name="password" icon={FiLock} placeholder="Senha" />
-            <Button name="submit">Entrar</Button>
-            <a href="forgot"> Esqueci minha senha</a>
+    const handleSubmit = useCallback( async (data: object) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                email: Yup.string().required('Email obrigatório').email("Digite um e-mail válido"),
+                password: Yup.string().required('Senha obrigatória'),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });
 
-        </form>
+        } catch (err) {
+            const errors = getValidationErrors(err);
 
-        <a href="forgot"><FiLogIn/> Criar conta</a>
-    </Content>
-    <Background />
-</Container>
+            formRef.current?.setErrors(errors);
+            console.log(errors);
+        }
 
-);
+    },[]);
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="Easy Barber"/>
+                <Form ref={formRef} onSubmit={handleSubmit}>
+                    <h1>Faça seu Logon</h1>
+
+                    <Input name="email" icon={FiMail} placeholder="E-Mail"/>
+                    <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
+                    <Button name="submit">Entrar</Button>
+                    <a href="forgot"> Esqueci minha senha</a>
+
+                </Form>
+
+                <a href="forgot"><FiLogIn/> Criar conta</a>
+            </Content>
+            <Background />
+        </Container>
+    );
+
+    };
 export default SignIn;
